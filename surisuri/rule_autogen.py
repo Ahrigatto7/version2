fe2d2f80933fae9237a5a85f45741b6d1d0d86ea
kyky <jeje6401@naver.com>
2025-06-17 02:24:45 +0900

    Delete surisuri directory

--- surisuri/rule_autogen.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def auto_add_rule(term, definition):
-    path = "data/term_rules.json"
-    with open(path, "r", encoding="utf-8") as f:
-        rules = json.load(f)
-    rules.append({"term": term, "definition": definition})
-    with open(path, "w", encoding="utf-8") as f:
-        json.dump(rules, f, ensure_ascii=False, indent=2)
